29da0763b94bf60ded69e76ff988808415664d90
Cory Anderson <cory.anderson6@yahoo.com>
2023-04-25 11:25:59 -0500

    components

--- src/components/CommentsForm/CommentsForm.jsx
+++ src/components/CommentsForm/CommentsForm.jsx
@@ -5,12 +5,29 @@ import { useHistory } from "react-router-dom";
 
 const CommentsForm = () => {
     const [comments, setCommentsValue] = useState("");
-    const disptach = useDispatch();
+    const dispatch = useDispatch();
     const history = useHistory();
 
     const handleSubmit = (event) => {
         event.preventDefault();
         dispatchEvent(setComments(comments));
         history.push("/review");
+
+        return (
+            <div>
+                <h2>Any comments you want to leave?</h2>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="comments">Comments:</label>
+                    <textarea
+                    id="comments"
+                    value={comments}
+                    onChange={(event) => setCommentsValue(event.target.value)}
+                    />
+                    <button type="submit">Next</button>
+                </form>
+            </div>
+        )
     };
-}
+};
+
+export default CommentsForm;
