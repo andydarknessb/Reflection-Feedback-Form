cdf6a9b55b4243d8a6311a3f2efebdbeb6739dfa
Cory Anderson <cory.anderson6@yahoo.com>
2023-04-25 11:20:44 -0500

    added functions

--- src/components/CommentsForm/CommentsForm.jsx
+++ src/components/CommentsForm/CommentsForm.jsx
@@ -3,3 +3,14 @@ import { useDispatch } from "react-redux";
 import { setComments } from "../../redux/slices/feedbackSlice";
 import { useHistory } from "react-router-dom";
 
+const CommentsForm = () => {
+    const [comments, setCommentsValue] = useState("");
+    const disptach = useDispatch();
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatchEvent(setComments(comments));
+        history.push("/review");
+    };
+}
