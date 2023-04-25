b11ac0208af3ce23a8e6a13532257b01569affe7
Cory Anderson <cory.anderson6@yahoo.com>
2023-04-25 10:35:00 -0500

    added handle submit function

--- src/components/UnderstandingForm/UnderstandingForm.jsx
+++ src/components/UnderstandingForm/UnderstandingForm.jsx
@@ -2,3 +2,19 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setUnderstanding } from "../../redux/slices/feedbackSlice";
 import { useHistory } from "react-router-dom";
+
+const UnderstandingForm = () => {
+    const [understanding, setUnderstandingValue] = useState("");
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(understanding >= 1 && understanding <= 5 {
+            dispatch(setUnderstanding)(understanding));
+            history.push("/support");
+    } else {
+        alert("Please provide a vald score between 1 and 5. ");
+     }
+    };
+    
