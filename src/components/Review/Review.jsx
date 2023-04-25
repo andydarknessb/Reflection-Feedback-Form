72915c65f69f7fabe4b21f2824a8f42430ce203b
Cory Anderson <cory.anderson6@yahoo.com>
2023-04-25 11:47:40 -0500

    updated review file

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { submitFeedback } from  "../../redux/slices/feedbackSlice";
 import axios from "axios";
 
-function Review() {
-    const feedback = useSelector((state) => state);
+const Review = () => {
+    const dispatch = useDispatch();
     const history = useHistory();
-
-    const handleSubmit = async () => {
-        try {
-            await axios.post("/api/feedback", feedback);
-            history.push("/success");
-        } catch (error) {
-            console.error("Error submitting feedback:", error);
-        }
-    };
+    const feedback = useSelector((state) => state.feedback);
 }
 
-return (
-    <div className={StyleSheet.container}>
-        <h2>Review Your Feedback</h2>
-        <div className={StyleSheet.feedback}>
-            <p>Feeling: {feedback.feeling}</p>
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (success) {
+            alert("Feedback submitted successfully!");
+            history.push("/");
+        } else {
+            alert("Failed to submit feedback.");
+        }
+    };
+
+
+    return (
+        <div>
+          <h2>Review Your Feedback</h2>
+          <ul>
+            <li>Feeling: {feedback.feeling}</li>
+            <li>Understanding: {feedback.understanding}</li>
+            <li>Support: {feedback.support}</li>
+            <li>Comments: {feedback.comments}</li>
+          </ul>
+          <button onClick={handleSubmit}>Submit</button>
         </div>
-        <button onClick={handleSubmit}>Submit Feedback</button>
-    </div>
-);
+      );
+    
+
 
 export default Review;
